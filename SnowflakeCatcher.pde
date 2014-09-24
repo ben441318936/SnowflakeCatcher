4773b4a4964de3e4d6354fc36dc71d343ecc50f5
unknown <jihuang65@697-334-Andunie.sf.k12.internal>
2014-09-24 10:43:06 -0700

    snow moving

--- SnowflakeCatcher.pde
+++ SnowflakeCatcher.pde
@@ -1,12 +1,13 @@
 SnowFlake storm [];
+int SnowFlakeArrayLength = 100;
 void setup()
 {
   //your code here
+  frameRate(10);
   size(500,500);
   background(0);
-  
-  storm = new SnowFlake [100];
-  for (int i=0; i <100; i++)
+  storm = new SnowFlake [SnowFlakeArrayLength];
+  for (int i=0; i < SnowFlakeArrayLength; i++)
   {
     storm[i] = new SnowFlake();
   }
@@ -14,49 +15,83 @@ void setup()
 void draw()
 {
   //your code here
-  for (int i=0; i <100; i++)
+  for (int i=0; i < SnowFlakeArrayLength; i++)
   {
+    storm[i].erase();
+    storm[i].reset();
+    storm[i].lookDown();
+    storm[i].move();
     storm[i].show();
   }
 }
 void mouseDragged()
 {
   //your code here
+  fill(255,0,0);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+
 }
 
 class SnowFlake
 {
   //class member variable declarations
-  int myX,myY,mySpeed;
+  int myX,myY;
+  boolean isMoving;
   SnowFlake()
   {
     //class member variable initializations
     myX = (int)(Math.random()*501);
-    myY = 0;
-    mySpeed = 1;
-
+    myY = (int)(Math.random()*501);
+    isMoving = true;
   }
   void show()
   {
     //your code here
+    //stroke(0);
+    noStroke();
+    fill(255);
     ellipse(myX,myY,10,10);
 
   }
   void lookDown()
   {
     //your code here
+    if(get(myX,myY+11) != color(0))
+    {
+      isMoving = false;
+    }
+    else 
+    {
+      isMoving = true;
+    }
   }
   void erase()
   {
     //your code here
+    fill(0);
+    stroke(0);
+    ellipse(myX, myY, 11, 11);
+
   }
   void move()
   {
     //your code here
+    if (isMoving == true)
+    {
+      myY++;
+    }
+    
   }
-  void wrap()
+  void reset()
   {
     //your code here
+    if(myY >= 500)
+    {
+      println(swag)
+      myY = 0;
+      myX = (int)(Math.random()*501);
+    }
   }
 }
 
--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -15,14 +15,15 @@ import java.io.IOException;
 public class SnowflakeCatcher extends PApplet {
 
 SnowFlake storm [];
+int SnowFlakeArrayLength = 100;
 public void setup()
 {
   //your code here
+  frameRate(10);
   size(500,500);
   background(0);
-  
-  storm = new SnowFlake [100];
-  for (int i=0; i <100; i++)
+  storm = new SnowFlake [SnowFlakeArrayLength];
+  for (int i=0; i < SnowFlakeArrayLength; i++)
   {
     storm[i] = new SnowFlake();
   }
@@ -30,49 +31,83 @@ public void setup()
 public void draw()
 {
   //your code here
-  for (int i=0; i <100; i++)
+  for (int i=0; i < SnowFlakeArrayLength; i++)
   {
+    storm[i].erase();
+    storm[i].reset();
+    storm[i].lookDown();
+    storm[i].move();
     storm[i].show();
+    println(i +" " + storm[i].isMoving);
   }
 }
 public void mouseDragged()
 {
   //your code here
+  fill(255,0,0);
+  noStroke();
+  ellipse(mouseX,mouseY,20,20);
+
 }
 
 class SnowFlake
 {
   //class member variable declarations
-  int myX,myY,mySpeed;
+  int myX,myY;
+  boolean isMoving;
   SnowFlake()
   {
     //class member variable initializations
     myX = (int)(Math.random()*501);
-    myY = 0;
-    mySpeed = 1;
-
+    myY = (int)(Math.random()*501);
+    isMoving = true;
   }
   public void show()
   {
     //your code here
+    //stroke(0);
+    noStroke();
+    fill(255);
     ellipse(myX,myY,10,10);
 
   }
   public void lookDown()
   {
     //your code here
+    if(get(myX,myY+11) != color(0))
+    {
+      isMoving = false;
+    }
+    else 
+    {
+      isMoving = true;
+    }
   }
   public void erase()
   {
     //your code here
+    fill(0);
+    stroke(0);
+    ellipse(myX, myY, 11, 11);
+
   }
   public void move()
   {
     //your code here
+    if (isMoving == true)
+    {
+      myY++;
+    }
+    
   }
-  public void wrap()
+  public void reset()
   {
     //your code here
+    if(myY >= 500)
+    {
+      myY = 0;
+      myX = (int)(Math.random()*501);
+    }
   }
 }
 
